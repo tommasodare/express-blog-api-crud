cf0cf3d8e7c741e8d2c4df53ffed5848a6f38e65
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-14 16:31:16 +0100

    Create Function Update, Modify and Destroy

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -11,3 +11,15 @@ function show(req, res) {
 function store(req, res) {
     res.send("Create new post")
 }
+
+function update(req, res) {
+    res.send(`Update the post with id: ${req.params.id}`)
+}
+
+function modify(req, res) {
+    res.send(`Modify the post with id: ${req.params.id}`)
+}
+
+function destroy(req, res) {
+    res.send(`Delete post with id: ${req.params.id}`)
+}
