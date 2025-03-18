5ff08952ce71a45b7b1c8928da58cc82c464d2a4
tommaso.dare <tommaso.dare96@gmail.com>
2025-03-18 12:51:17 +0100

    Add Middleware for route not found

--- blog_server.js
+++ blog_server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const errorHandler = require('./middlewares/serverError')
+const routeNotFound = require('./middlewares/routeNotFound')
 
 // Inserimento body-parser
 app.use(express.json())
@@ -18,5 +19,8 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 })
 
-// Registro il middleware
+// Registro il middleware per la gestione errori
 app.use(errorHandler)
+
+// Registro il middleware per rotta inesistente
+app.use(routeNotFound)
